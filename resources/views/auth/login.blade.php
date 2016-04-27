fb95ee622e304f2ab5e5115b57e5de1c4d612e1d
Emmanuel <nondefyde@gmail.com>
2016-04-27 13:48:18 +0100

    db

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,42 +10,46 @@ Like: www.facebook.com/keenthemes
 Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
 License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
 -->
-<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
-<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if IE 8]>
+<html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]>
+<html lang="en" class="ie9 no-js"> <![endif]-->
 <!--[if !IE]><!-->
 <html lang="en">
 <!--<![endif]-->
 <!-- BEGIN HEAD -->
 
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <title>Metronic | User Login 5</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta content="width=device-width, initial-scale=1" name="viewport" />
-    <meta content="" name="description" />
-    <meta content="" name="author" />
+    <meta content="width=device-width, initial-scale=1" name="viewport"/>
+    <meta content="" name="description"/>
+    <meta content="" name="author"/>
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet"
+          type="text/css"/>
+    <link href="/assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/global/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css"/>
     <!-- END GLOBAL MANDATORY STYLES -->
     <!-- BEGIN PAGE LEVEL PLUGINS -->
-    <link href="/assets/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="/assets/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css"/>
     <!-- END PAGE LEVEL PLUGINS -->
     <!-- BEGIN THEME GLOBAL STYLES -->
-    <link href="/assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
-    <link href="/assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+    <link href="/assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css"/>
+    <link href="/assets/global/css/plugins.min.css" rel="stylesheet" type="text/css"/>
     <!-- END THEME GLOBAL STYLES -->
     <!-- BEGIN PAGE LEVEL STYLES -->
-    <link href="/assets/pages/css/login-5.min.css" rel="stylesheet" type="text/css" />
+    <link href="/assets/pages/css/login-5.min.css" rel="stylesheet" type="text/css"/>
     <!-- END PAGE LEVEL STYLES -->
     <!-- BEGIN THEME LAYOUT STYLES -->
     <!-- END THEME LAYOUT STYLES -->
-    <link rel="shortcut icon" href="favicon.ico" /> </head>
+    <link rel="shortcut icon" href="favicon.ico"/>
+</head>
 <!-- END HEAD -->
 
 <body class=" login">
@@ -54,20 +58,27 @@ License: You must have a valid license purchased only from themeforest(the above
     <div class="row bs-reset">
         <div class="col-md-6 bs-reset">
             <div class="login-bg" style="background-image:url(/assets/pages/img/login/bg1.jpg)">
-                <img class="login-logo" src="/assets/pages/img/login/logo.png" /> </div>
+                <img class="login-logo" src="/assets/pages/img/login/logo.png"/></div>
         </div>
         <div class="col-md-6 login-container bs-reset">
             <div class="login-content">
-                <h1>Metronic Admin Login</h1>
-                <p> Lorem ipsum dolor sit amet, coectetuer adipiscing elit sed diam nonummy et nibh euismod aliquam erat volutpat. Lorem ipsum dolor sit amet, coectetuer adipiscing. </p>
+                <h1>Smart School Login</h1>
+
+                <p> There is one quality that one must possess to win, and that is definiteness of purpose, the
+                    knowledge of what one wants and a burning desire to possess it.</p>
+
                 <form action="{{ url('/auth/login') }}" class="login-form" method="post">
                     @include('errors.errors')
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
                     <div class="row">
                         <div class="col-xs-6">
-                            <input class="form-control form-control-solid placeholder-no-fix form-group" value="{{ old('email') }}" type="email" autocomplete="off" placeholder="Email" name="email" required/> </div>
+                            <input class="form-control form-control-solid placeholder-no-fix form-group"
+                                   value="{{ old('email') }}" type="email" autocomplete="off" placeholder="Email"
+                                   name="email" required/></div>
                         <div class="col-xs-6">
-                            <input class="form-control form-control-solid placeholder-no-fix form-group" type="password" autocomplete="off" placeholder="Password" name="password" required/> </div>
+                            <input class="form-control form-control-solid placeholder-no-fix form-group" type="password"
+                                   autocomplete="off" placeholder="Password" name="password" required/></div>
                     </div>
                     <div class="row">
                         <div class="col-sm-4">
@@ -79,7 +90,8 @@ License: You must have a valid license purchased only from themeforest(the above
                         </div>
                         <div class="col-sm-8 text-right">
                             <div class="forgot-password">
-                                <a href="{{ url('auth/forget-password') }}" id="forget-password" class="forget-password">Forgot Password?</a>
+                                <a href="{{ url('auth/forget-password') }}" id="forget-password"
+                                   class="forget-password">Forgot Password?</a>
                             </div>
                             <button class="btn blue" type="submit">Sign In</button>
                         </div>
@@ -88,9 +100,12 @@ License: You must have a valid license purchased only from themeforest(the above
                 <!-- BEGIN FORGOT PASSWORD FORM -->
                 <form class="forget-form" action="/reset-password" method="post">
                     <h3 class="font-green">Forgot Password ?</h3>
+
                     <p> Enter your e-mail address below to reset your password. </p>
+
                     <div class="form-group">
-                        <input class="form-control placeholder-no-fix form-group" type="text" autocomplete="off" placeholder="Email" name="email" /> </div>
+                        <input class="form-control placeholder-no-fix form-group" type="text" autocomplete="off"
+                               placeholder="Email" name="email"/></div>
                     <div class="form-actions">
                         <button type="button" id="back-btn" class="btn grey btn-default">Back</button>
                         <button type="submit" class="btn blue btn-success uppercase pull-right">Submit</button>
@@ -133,7 +148,8 @@ License: You must have a valid license purchased only from themeforest(the above
 <script src="/assets/global/plugins/jquery.min.js" type="text/javascript"></script>
 <script src="/assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 <script src="/assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
-<script src="/assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+<script src="/assets/global/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js"
+        type="text/javascript"></script>
 <script src="/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
 <script src="/assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
 <script src="/assets/global/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
