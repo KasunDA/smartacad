61b7a75f011f2488ba38df98d4218e7bed86171c
Emmanuel <nondefyde@gmail.com>
2016-04-28 13:44:47 +0100

    db

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@ License: You must have a valid license purchased only from themeforest(the above
 
 <head>
     <meta charset="utf-8"/>
-    <title>Metronic | User Login 5</title>
+    <title>Smart School | User Login</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1" name="viewport"/>
     <meta content="" name="description"/>
