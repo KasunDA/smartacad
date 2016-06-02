058d325712b73347b7a1bcb40a511f79573bfc85
Kingsley Chinaka <kingsley4united@yahoo.com>
2016-06-02 15:45:13 +0100

    Dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -64,7 +64,9 @@ class DashboardController extends Controller
     }
 
     public function getStaff(){
-        $staffs = User::where('user_type_id', 1)->get();
+        $url = "http://107.20.195.151/mcast_ws/?user=$user&password=$password&from=Kheengz&to=2348030734377&message=message_testing";
+        $ret = file($url);
+//        $staffs = User::where('user_type_id', 1)->get();
         $temp = '';
 //        $staffs = User::where('user_type_id', Staff::USER_TYPE)->get();
 //        foreach($staffs as $staff){
@@ -75,10 +77,10 @@ class DashboardController extends Controller
 ////            $this->sendSMS($msg, $staff->phone_no);
 //
 //        }
-        $msg = 'Username: 08022020075 or kheengz@gmail.com';
-        $msg .= ' and Password: password then visit this link to login via portal.solidsteps.org' ;
-        $temp .= $this->sendSMS($msg, '08022020075')[0];
-        $temp = $temp . '<br>' . $this->sendSMS($msg, '08030737377')[0];
-        return response()->json($temp);
+//        $msg = 'Username: 08022020075 or kheengz@gmail.com';
+//        $msg .= ' and Password: password then visit this link to login via portal.solidsteps.org' ;
+//        $temp .= $this->sendSMS($msg, '08022020075')[0];
+//        $temp = $temp . '<br>' . $this->sendSMS($msg, '08030737377')[0];
+        return response()->json($ret);
     }
 }
