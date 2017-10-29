f3cec390bf4a5e97c4c6ce87272016c7c5cedd15
Kingsley Chinaka <kingsley4united@yahoo.com>
2017-10-29 15:28:43 +0100

    validate to check migration if table exist

--- database/migrations/2017_10_26_115645_create_banks_account_table.php
+++ database/migrations/2017_10_26_115645_create_banks_account_table.php
@@ -12,26 +12,30 @@ class CreateBanksAccountTable extends Migration
      */
     public function up()
     {
-        Schema::connection('admin_mysql')->create('banks', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('code', 10);
-            $table->tinyInteger('active', false, true)->index()->default(1);
-            $table->timestamps();
-            $table->softDeletes();
-        });
+        if (Schema::connection('admin_mysql')->hasTable('banks')) {
+            Schema::connection('admin_mysql')->create('banks', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->string('code', 10);
+                $table->tinyInteger('active', false, true)->index()->default(1);
+                $table->timestamps();
+                $table->softDeletes();
+            });
+        }
 
-        Schema::connection('admin_mysql')->create('school_banks', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('account_name');
-            $table->string('account_number');
-            $table->tinyInteger('active', false, true)->index()->default(1);
-            $table->integer('bank_id', false, true)->index();
-            $table->integer('classgroup_id', false, true)->index();
-            $table->integer('school_id', false, true)->index();
-            $table->timestamps();
-            $table->softDeletes();
-        });
+        if (Schema::connection('admin_mysql')->hasTable('school_banks')) {
+            Schema::connection('admin_mysql')->create('school_banks', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('account_name');
+                $table->string('account_number');
+                $table->tinyInteger('active', false, true)->index()->default(1);
+                $table->integer('bank_id', false, true)->index();
+                $table->integer('classgroup_id', false, true)->index();
+                $table->integer('school_id', false, true)->index();
+                $table->timestamps();
+                $table->softDeletes();
+            });
+        }
     }
 
     /**
@@ -41,7 +45,11 @@ class CreateBanksAccountTable extends Migration
      */
     public function down()
     {
-        Schema::connection('admin_mysql')->drop('school_banks');
-        Schema::connection('admin_mysql')->drop('banks');
+        if (Schema::connection('admin_mysql')->hasTable('school_banks')) {
+            Schema::connection('admin_mysql')->drop('school_banks');
+        }
+        if (Schema::connection('admin_mysql')->hasTable('banks')) {
+            Schema::connection('admin_mysql')->drop('banks');
+        }
     }
 }
