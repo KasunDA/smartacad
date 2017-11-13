38a4a2e3eac4f3993609b9d7722692efab5118e4
Kingsley Chinaka <kingsley4united@yahoo.com>
2017-11-13 12:49:20 +0100

    exception and kernel migration to 5.3

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -18,6 +18,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Illuminate\Auth\AuthenticationException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
@@ -142,5 +143,21 @@ class Handler extends ExceptionHandler
         ////////////////////////////////////////////////// end: KHEENGZ CUSTOM CODE ///////////////////////////////////////
 
         return parent::render($request, $e);
+    }
+
+    /**
+     * Convert an authentication exception into an unauthenticated response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+
+        return redirect()->guest('login');
     }
 }
